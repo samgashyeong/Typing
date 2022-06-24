716e132efb4c5b11e5da81652e9aa84e06600371
whdbcks111 <81742754+whdbcks111@users.noreply.github.com>
2022-06-25 02:20:42 +0900

    게임 텍스트 추가 및 로직 변경

---
+++ app/src/main/java/com/example/typing/view/util/KoreanSeparator.kt
@@ -0,0 +1,25 @@
+package com.example.typing.view.util
+
+import java.lang.StringBuilder
+
+class KoreanSeparator {
+    companion object {
+        private val cho = arrayOf("ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ")
+        private val jung = arrayOf("ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ")
+        private val jong = arrayOf("", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ",
+            "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ")
+
+        fun separate(str: String): String {
+            val builder = StringBuilder()
+            for (c in str) {
+                if (c in '가'..'힣') {
+                    builder.append(cho[(c - '가') / 28 / 21])
+                    builder.append(jung[(c - '가') / 28 % 21])
+                    builder.append(jong[(c - '가') % 28])
+                }
+                else builder.append(c)
+            }
+            return builder.toString()
+        }
+    }
+}
